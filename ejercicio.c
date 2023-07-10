863744613ad19af77a6245d3e03830adbd046541
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2023-07-10 15:46:40 -0500

    Agregue una funciòn llamada leertexto para abrir el archivo alumnos.txt , y despues llamo a la funciòn en la funciòn principal.
    Con un for lo que hago es que vaya leyendo los datos dentro del texto de las 5 personas que estàn en este.
    La estructura no se subiò bien en el anterior commit por alguna razòn pero puse los datos segùn observe en el texto y las variables de tipo char las defini con un arreglo para el espacio de la cadena que entrara.

--- ejercicio.c
+++ ejercicio.c
@@ -1,8 +1,40 @@
+/*Construya un programa que lea los datos de un archivo plano que contiene los datos de un conjunto
+ de estudiantes, ordenados en función de su código, y genere un nuevo archivo ordenado de mayor a menor 
+ e incluya el cálculo de la nota promedio.*/
+
 #include <stdio.h>
+ 
+ struct estudiante{
+    char nombre[40];
+    char carrera[60];
+    int promedio1;
+    int promedio2;
+    int promedio3;
+    int promediofinal;
+ };
+
+void leertexto(struct estudiante estudiantes[5])
+{
+    FILE*archivo;
+    archivo=fopen("alumnos.txt","r");
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo.\n");
+        return;
+    }
+
+   for (int i=0;i<5;i++)
+   {
+    fscanf(archivo, "%s;%s;%d;%d;%d", estudiantes[i].nombre, estudiantes[i].carrera,estudiantes[i].promedio1,estudiantes[i].promedio2,estudiantes[i].promedio3);
+   }
+    
+
+    fclose(archivo);
+}
+
 int main ()
 {
-
-
+ struct estudiante estudiantes[5];
+leertexto(estudiantes);
 
     return 0;
 }
